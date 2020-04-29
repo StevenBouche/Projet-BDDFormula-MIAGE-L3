7ab3192db527faef43e8db3e21b4e00b97d09cd9
steven <steven.bouche@outlook.com>
2020-04-30 00:36:00 +0200

    update

---
+++ script/insert_racing_stable_pilote_history.sql
@@ -0,0 +1,7 @@
+--------------------------------------------------------
+--  Fichier créé - jeudi-avril-30-2020   
+--------------------------------------------------------
+REM INSERTING into BS708164LM20.RACINGSTABLEPILOTEHISTORY
+SET DEFINE OFF;
+Insert into BS708164LM20.RACINGSTABLEPILOTEHISTORY (IDRACINGSTABLEPILOTEHISTORY,HADJOINED,HADLEFT,IDRACINGSTABLE,IDPILOTE) values ('1',to_date('06/02/19','DD/MM/RR'),to_date('14/08/19','DD/MM/RR'),'3','7');
+Insert into BS708164LM20.RACINGSTABLEPILOTEHISTORY (IDRACINGSTABLEPILOTEHISTORY,HADJOINED,HADLEFT,IDRACINGSTABLE,IDPILOTE) values ('4',to_date('06/02/19','DD/MM/RR'),to_date('14/08/19','DD/MM/RR'),'6','8');
